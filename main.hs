e90b8d0ef5ea1856715e78d43dc3cb0c9bad1e84
Harrison Brewton <brewton@rockhopper-08.cs.wisc.edu>
2016-09-28 20:11:28 -0500

    Initial commit

---
+++ main.hs
@@ -0,0 +1,15 @@
+module Main (main) where
+
+import Tokenizer
+import System.Environment
+
+compile = show . Tokenizer.tokenize
+
+
+
+main = do
+	args <- getArgs
+	text <- readFile $ foldr (++) "" args
+	putStrLn $ compile text
+
+
---
+++ test.fp
@@ -0,0 +1,3 @@
+(defun (square x)
+	(* x x)
+)
